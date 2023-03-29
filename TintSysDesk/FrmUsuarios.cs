34ddd2cca8def76b0693e1c9bfa143a28f179a72
WELLINGTON VIEIRA DOS SANTOS <wellington.vsantos@gmail.com>
2023-03-29 13:56:14 -0300

    confirma

---
+++ TintSysDesk/FrmUsuarios.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TintSysClass;
+
+namespace TintSysDesk
+{
+    public partial class FrmUsuarios : Form
+    {
+        public FrmUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Usuario usuario = new Usuario(
+                txtNome.Text,txtEmail.Text,txtSenha.Text,
+                Nivel.ObterPorId(Convert.ToInt32(comboBox1.SelectedValue)));
+            usuario.Inserir();
+            txtId.Text = usuario.Id.ToString(); 
+        }
+
+        private void FrmUsuarios_Load(object sender, EventArgs e)
+        {
+            comboBox1.DataSource = Nivel.Listar();
+            comboBox1.ValueMember = "Id";
+            comboBox1.DisplayMember= "Nome";
+        }
+    }
+}
