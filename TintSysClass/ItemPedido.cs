5446cf45babb530b059d1ed191e8b157c0da9a4d
WELLINGTON VIEIRA DOS SANTOS <wellington.vsantos@gmail.com>
2023-04-13 15:02:02 -0300

    alterado itempedido

--- TintSysClass/ItemPedido.cs
+++ TintSysClass/ItemPedido.cs
@@ -51,19 +51,43 @@ namespace TintSysClass
         }
         public static ItemPedido BuscarPorProdutoPedido(int pedido_id, int produto_id)
         {
-            ItemPedido iten = new ItemPedido();
+            ItemPedido item = new ItemPedido();
             var cmd = Banco.Abrir();
-            cmd.CommandText = "select * from itempedido where pedido_id = " + pedido_id;
+            cmd.CommandText = "select * from itempedido " +
+                "where pedido_id = @pedido and produto_id = @produto";
+            cmd.Parameters.Add("@pedido", MySqlDbType.Int32).Value = pedido_id;
+            cmd.Parameters.Add("@produto", MySqlDbType.Int32).Value = produto_id;
             var dr = cmd.ExecuteReader();
             while (dr.Read())
             {
-                iten.Id = dr.GetInt32(0);
-                iten.Produto = Produto.ObterPorId(dr.GetInt32(2));
-                iten.Preco = dr.GetDouble(3);
-                iten.Quantidade = dr.GetDouble(4);
-                iten.Desconto = dr.GetDouble(5);
+                item.Id = dr.GetInt32(0);
+                item.Produto = Produto.ObterPorId(dr.GetInt32(2));
+                item.Preco = dr.GetDouble(3);
+                item.Quantidade = dr.GetDouble(4);
+                item.Desconto = dr.GetDouble(5);
             }
-            return iten;
+            return item;
+        }
+        public static List<ItemPedido> ListarPorPedido(int pedido_id)
+        {
+            ItemPedido item = null;
+            List<ItemPedido> itens = new List<ItemPedido>();
+            var cmd = Banco.Abrir();
+            cmd.CommandText = "select * from itempedido " +
+                "where pedido_id = @pedido";
+            cmd.Parameters.Add("@pedido", MySqlDbType.Int32).Value = pedido_id;
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                item = new ItemPedido();
+                item.Id = dr.GetInt32(0);
+                item.Produto = Produto.ObterPorId(dr.GetInt32(2));
+                item.Preco = dr.GetDouble(3);
+                item.Quantidade = dr.GetDouble(4);
+                item.Desconto = dr.GetDouble(5);
+                itens.Add(item);
+            }
+            return itens;
         }
 
     }
